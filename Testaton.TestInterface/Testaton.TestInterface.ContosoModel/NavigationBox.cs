4882cbdc052062fe3c16d4355e8dce5838b34f28
PioterB <piotr@barankiewicz.tech>
2020-03-28 12:24:31 +0100

    Part 2: contoso model stub

---
+++ Testaton.TestInterface/Testaton.TestInterface.ContosoModel/NavigationBox.cs
@@ -0,0 +1,21 @@
+﻿using Bumblebee.Implementation;
+using Bumblebee.Interfaces;
+using OpenQA.Selenium;
+
+namespace Testaton.TestInterface.ContosoModel
+{
+    /// <summary>
+    /// Represents application Navigation.
+    /// </summary>
+    public class NavigationBox : Block
+    {
+        public NavigationBox(IBlock parent) : base(parent, By.XPath("//nav//ul"))
+        {
+        }
+
+        /// <summary>
+        /// Gets access to home page.
+        /// </summary>
+        public IClickable<ShellPage> Home => new Clickable<ShellPage>(this, By.XPath("li[1]"));
+    }
+}
---
+++ Testaton.TestInterface/Testaton.TestInterface.ContosoModel/ShellPage.cs
@@ -0,0 +1,22 @@
+﻿using Bumblebee.Implementation;
+using Bumblebee.Setup;
+
+namespace Testaton.TestInterface.ContosoModel
+{
+    /// <summary>
+    /// Represents main shell which hosts other parts of application. 
+    /// Shell contains main menu and global actions.
+    /// </summary>
+    public class ShellPage : Page
+    {
+        public ShellPage(Session session) : base(session)
+        {
+            Navigation = new NavigationBox(this);
+        }
+
+        /// <summary>
+        /// Gets application's navigation menu.
+        /// </summary>
+        public NavigationBox Navigation { get; }
+    }
+}
